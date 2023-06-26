1cd87747221d8cd23acff6ef5042b78d8250c799
DarrenDonovan <donovandarren12@gmail.com>
2023-06-26 12:39:04 +0700

    konten modal

--- Project Zethro/scriptmodal.js
+++ Project Zethro/scriptmodal.js
@@ -1,26 +1,26 @@
-// Modal
-// Get the modal
-var modal = document.getElementById("myModal");
+// Get the modal element
+const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("mybutton");
+// Get all the image elements that trigger the modal
+const modalTriggers = document.querySelectorAll(".modal-trigger");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the close button element
+const closeButton = document.querySelector(".close");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+// Function to open the modal
+function openModal() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+// Function to close the modal
+function closeModal() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// Add click event listeners to all the image triggers
+modalTriggers.forEach(function(trigger) {
+  trigger.addEventListener("click", openModal);
+});
+
+// Add a click event listener to the close button
+closeButton.addEventListener("click", closeModal);
